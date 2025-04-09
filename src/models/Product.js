65354805aec8801dfbac5723f4d80dbbf965dfc5
Gildácio Lopes <gildacioj399@gmail.com>
2025-04-09 11:24:23 -0300

    feat: create new models for mongoDB

---
+++ src/models/Product.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: String,
+    price: { type: Number, required: true },
+    countInStock: { type: Number, default: 0 },
+    image: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
